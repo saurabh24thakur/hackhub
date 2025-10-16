0ce1a01cb9d4b7c742c6aa5ead434b1aafa0dbd8
saurabh5532u <saurabh5532u@gmail.com>
2025-10-16 16:56:53 +0000

    adding vercel bkd

--- api/index.js
+++ api/index.js
@@ -19,6 +19,4 @@ app.use('/api/hackathons', hackathonRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+module.exports = app;
