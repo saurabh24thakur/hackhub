4e5f9b29dfc412e9bf4ce735e081205d828a398f
saurabh24thakur <saurabh5532u@gmail.com>
2025-10-22 21:53:15 +0530

    cors error fix

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Use CORS
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://hackhubfinal.vercel.app'],
   credentials: true,
 };
 app.use(cors(corsOptions));
